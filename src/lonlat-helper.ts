d29869e20ed9986958561cd746ccb24fd133d079
duesingu <uwe.duesing@niwa.co.nz>
2019-12-09 16:38:59 +1300

    rework to accomodate the entering lon lat or lat lon (for New Zeland anyway)

--- src/lonlat-helper.ts
+++ src/lonlat-helper.ts
@@ -6,36 +6,6 @@ export class LonlatHelper {
 
     private lonlatregex = '';
 
-    // public isLonLat = (exp): boolean => {
-    //     let validLonlat: boolean;
-    //     // checking whether we have a valid lonlat e.g 192.45 -36
-    //     const regexres = exp.match(/(\-*1*[0-9]*\.*[0-9]*[WEwe]*)[\s,](\s*\-*[0-9]*\.*[0-9]*[NSns]*)/)
-    //     console.log(regexres);
-    //     if (regexres !== null && regexres[1] !== '' && regexres[2] !== '') {
-    //         // an invalid option has been detected as the user entered a negative lon but also used 'E' or 'W'
-    //         if (regexres[1].indexOf('-') !== -1 && regexres[1].indexOf('W') !== -1 || (regexres[1].indexOf('E') !== -1)) {
-    //
-    //             console.log('false');
-    //             validLonlat = false;
-    //         } else {
-    //             console.log('true');
-    //             validLonlat = true;
-    //         }
-    //
-    //         // an invalid option has been detected as the user entered a negative lon but also used 'E' or 'W'
-    //         if (regexres[2].indexOf('-') !== -1 && (regexres[2].indexOf('S') !== -1 || regexres[2].indexOf('N') !== -1)) {
-    //             validLonlat = false;
-    //         } else {
-    //             validLonlat = true;
-    //         }
-    //
-    //     } else {
-    //         validLonlat = false;
-    //     }
-    //     return validLonlat;
-    // }
-
-
     public getLonLat = (exp): LonLat => {
 
 
@@ -47,7 +17,6 @@ export class LonlatHelper {
         let validLonlat: boolean;
         // checking whether we have a valid lonlat e.g 192.45 -36
         const regexres = exp.match(/(\-*1*[0-9]*\.*[0-9]*[NSnsWEwe]*)[\s,](\s*\-*[0-9]*\.*[0-9]*[NSnsWEwe]*)/)
-        console.log('regexres', regexres);
         if (regexres !== null && regexres[1] !== '' && regexres[2] !== '') {
 
 
@@ -58,8 +27,6 @@ export class LonlatHelper {
             } else {
                 // the actual format is correct - now we need to see whether n,s,e,w is present ...
                 const values = [regexres[1], regexres[2]];
-                console.log('reg1',regexres[1]);
-                console.log('reg2',regexres[2]);
                 values.forEach((value, index) => {
                     if (this.directionPresent(value)) {
 
@@ -67,7 +34,6 @@ export class LonlatHelper {
                         if (this.directionValuePlausible(valueAndDirection[1], valueAndDirection[2])) {
 
                             if (dir1 !== undefined && dir1 === valueAndDirection[2]) {
-                                console.error('invalt value: same direction error');
                                 lon = undefined;
                                 lat = undefined;
 
@@ -140,7 +106,6 @@ export class LonlatHelper {
     }
 
     public latitudePlausible = (lat) => {
-        console.log('checkingLat', lat);
         if (lat >= -90 && lat <= 90) {
             return true;
         } else {
@@ -149,7 +114,6 @@ export class LonlatHelper {
     }
 
     public longitudePlausible = (lon) => {
-        console.log('checkingLon', lon);
         if (lon >= -180 && lon <= 180) {
             return true;
         } else {
@@ -159,7 +123,6 @@ export class LonlatHelper {
 
 
     public directionValuePlausible(value, direction) {
-        console.log(value, direction);
         if (direction === 'w' || direction === 'e') {
             if (value >= 0 && value <= 180) {
                 return true
@@ -174,14 +137,12 @@ export class LonlatHelper {
                 return false;
             }
         }
-        console.log('XXX');
         return false;
     }
 
 
     private directionPresent(exp) {
         if (exp.toLowerCase().indexOf('w') !== -1 || (exp.toLowerCase().indexOf('e') !== -1) || exp.toLowerCase().indexOf('n') !== -1 || exp.toLowerCase().indexOf('s') !== -1) {
-            console.log('dir!');
             return true
         } else {
             return false;
@@ -211,41 +172,7 @@ export class LonlatHelper {
         return longitude;
     }
 
-    // public getLonLat = (exp): LonLat => {
-    //
-    //     let lon: number;
-    //     let lat: number;
-    //     // console.log('res', this.isLonLat(exp));
-    //     if (this.isLonLat(exp)) {
-    //
-    //         const regexres = exp.match(/(\-*1*[0-9]*\.*[0-9]*[WEwe]*)[\s,](\s*\-*[0-9]*\.*[0-9]*[NSns]*)/);
-    //         const tempLon = regexres[1].toLowerCase();
-    //         const tempLat = regexres[2].toLowerCase();
-    //
-    //         if (tempLon.indexOf('e') !== -1) {
-    //             lon = parseFloat(tempLon.substr(0, tempLon.indexOf('e')));
-    //         } else if (tempLon.indexOf('w') !== -1) {
-    //             lon = 0 - parseFloat(tempLon.substr(0, tempLon.indexOf('w')));
-    //         } else {
-    //             lon = parseFloat(tempLon);
-    //         }
-    //
-    //
-    //         if (tempLat.indexOf('n') !== -1) {
-    //             lat = parseFloat(tempLat.substr(0, tempLat.indexOf('n')));
-    //         } else if (tempLat.indexOf('s') !== -1) {
-    //             lat = 0 - parseFloat(tempLat.substr(0, tempLat.indexOf('s')));
-    //         } else {
-    //             lat = parseFloat(tempLat);
-    //         }
-    //         if (lon > 180 || lon < -180 || lat > 90 || lat < -90) {
-    //             return null;
-    //         }
-    //         return new LonLat(lon, lat)
-    //     } else {
-    //         return null;
-    //     }
-    // }
+
 
     public boundingBoxtoExtent = (boundingBox) => {
         return [parseFloat(boundingBox[2]), parseFloat(boundingBox[0]), parseFloat(boundingBox[3]), parseFloat(boundingBox[1])];
